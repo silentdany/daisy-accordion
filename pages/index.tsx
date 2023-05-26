4c9d0fa44d9278184517bc00dc2747c5a6b0b50c
silentdany <dagnicourt.a@gmail.com>
2023-05-26 11:04:36 +0200

    feat: updated hero, almost final form ?

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,12 @@ import Image from "next/image";
 import Link from "next/link";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import SquigglyLines from "../components/SquigglyLines";
 import { Testimonials } from "../components/Testimonials";
-import { Transition } from "@headlessui/react";
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   ArrowTopRightOnSquareIcon,
-  ChatBubbleBottomCenterIcon,
   ChatBubbleBottomCenterTextIcon,
-  ChevronDoubleDownIcon,
-  ClipboardDocumentCheckIcon,
   ClipboardDocumentListIcon,
-  ClockIcon,
   Cog6ToothIcon,
   RocketLaunchIcon,
   SparklesIcon,
@@ -28,9 +22,9 @@ type CTAButtonProps = {
 const CTAButton = ({ title }: CTAButtonProps) => (
   <Link
     href="#"
-    className="self-center animate-border w-max inline-block hover:shadow-xl hover:translate-x-2 rounded-md bg-white bg-gradient-to-r shadow-sm duration-100 from-primary-500 via-secondary-500 to-tertiary-500 bg-[length:400%_400%] p-2"
+    className="self-center animate-border w-max inline-block hover:shadow-xl hover:translate-x-2 rounded-md bg-white bg-gradient-to-r shadow-sm duration-100 from-primary-500 via-secondary-500 to-tertiary-500 bg-[length:400%_400%] p-1"
   >
-    <span className="flex items-center justify-center px-8 py-4 text-2xl font-bold rounded-md bg-primary-50 text-neutral-900">
+    <span className="flex items-center justify-center px-4 py-2 text-xl font-bold rounded-md bg-primary-50 text-neutral-900">
       {/* Boost your{" "}
               <span className="font-bold text-primary-500">product</span>
               ivity */}
@@ -39,21 +33,6 @@ const CTAButton = ({ title }: CTAButtonProps) => (
     </span>
   </Link>
 );
-
-// a function set sets alternatively true and false every 2 seconds
-const useInfinite = (initialValue: boolean, timeout: number) => {
-  const [value, setValue] = useState(initialValue);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setValue(false);
-      setTimeout(() => {
-        setValue(true);
-      }, 1);
-    }, timeout);
-    return () => clearInterval(interval);
-  }, []);
-  return value;
-};
 
 const Home: NextPage = () => {
   return (
@@ -81,98 +60,94 @@ const Home: NextPage = () => {
               </h2>
             </div>
           </div>
-          {/* <div className="relative w-[95%] bg-gray-200 -z-10">
-            <div className="absolute w-[95%] top-0 h-4 mx-4 progress overflow-hidden bg-primary-500"></div>
-          </div> */}
           <div className="relative flex justify-between w-full">
-            <Image
-              alt="Original photo of a room"
-              src="/packshot.png"
-              className="absolute z-10 object-contain w-1/3 h-96"
-              width={373}
-              height={373}
-            />
-            <Transition
-              as={Fragment}
-              appear={true}
-              show={useInfinite(true, 3000)}
-              enter="transition ease-out duration-[2000ms] infinite"
-              enterFrom="transform opacity-100 scale-100 translate-x-0"
-              enterTo="transform opacity-0 scale-75 translate-x-64 rotate-[30deg]"
-            >
-              <Image
-                alt="Original photo of a room"
-                src="/packshot.png"
-                className="z-0 object-contain w-1/3 h-96"
-                width={373}
-                height={373}
-              />
-            </Transition>
-            <div className="relative flex items-center justify-center w-1/3">
+            <div className="flex justify-center w-4/12">
+              <div className="w-5/6 rounded-xl bg-neutral-50 h-96 shadow-2xl">
+                <div className="z-10 flex flex-col justify-between w-full p-2 space-y-2 font-mono text-sm leading-4 text-left h-96">
+                  <div className="flex w-full h-2 space-x-1">
+                    <span className="w-2 h-2 bg-red-500 rounded-full"></span>
+                    <span className="w-2 h-2 bg-yellow-500 rounded-full"></span>
+                    <span className="w-2 h-2 bg-green-500 rounded-full"></span>
+                  </div>
+                  <Image
+                    alt="Original photo of a room"
+                    src="/packshot.png"
+                    className="z-10 object-contain h-96 bg-gradient-to-b from-neutral-200 to-slate-400 rounded-xl"
+                    width={373}
+                    height={373}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="w-1/12 flex items-center justify-start h-full">
+              <div className="lds-ellipsis absolute right-0 ">
+                <div className="bg-primary-500"></div>
+                <div className="bg-primary-500/75"></div>
+                <div className="bg-primary-500/50"></div>
+                <div className="bg-primary-500/25"></div>
+              </div>
+            </div>
+            <div className="relative flex items-center justify-center w-3/12">
               <Image
                 alt="Laptop"
                 src="/laptop.png"
-                className="absolute object-contain rounded-full shadow-2xl bg-gradient-to-br from-primary-500/50 via-secondary-500/50 to-tertiary-500/50 h-96"
+                className="absolute object-contain rounded-full shadow-2xl bg-gradient-to-br from-primary-500/50 via-secondary-500/50 to-tertiary-500/50 h-72"
                 width={373}
                 height={373}
               />
-              <div className="-mt-3 inline-block absolute h-20 w-20 border-secondary-500 animate-spin rounded-full border-4 border-solid border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" />
-              <div className="-mt-3 inline-block absolute h-24 w-24 border-tertiary-500 animate-[spin_1.5s_linear_infinite] rounded-full border-4 border-solid border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" />
-              <div className="-mt-3 inline-block absolute h-28 w-28 border-primary-500 animate-[spin_2s_linear_infinite] rounded-full border-4 border-solid border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" />
+              <div className="-mt-3 inline-block absolute h-12 w-12 border-secondary-500 animate-[spin_2s_linear_infinite] rounded-full border-4 border-solid border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" />
+              <div className="-mt-3 inline-block absolute h-16 w-16 border-tertiary-500 animate-[spin_2.5s_linear_infinite] rounded-full border-4 border-solid border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" />
+              <div className="-mt-3 inline-block absolute h-20 w-20 border-primary-500 animate-[spin_3s_linear_infinite] rounded-full border-4 border-solid border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" />
             </div>
-            <div className="flex justify-center w-1/3">
-              <div className="w-5/6 rounded-xl bg-neutral-50 h-96">
-                <Transition
-                  as={Fragment}
-                  appear={true}
-                  show={useInfinite(true, 3000)}
-                  enter="transition ease-out duration-[2000ms] infinite delay-[2000ms]"
-                  enterFrom="transform opacity-0 scale-75 -translate-x-64"
-                  enterTo="transform opacity-100 scale-100 translate-x-0"
-                >
-                  <div className="z-10 flex flex-col justify-between w-full p-4 space-y-2 font-mono text-sm leading-4 text-left h-96">
-                    <div className="flex w-full h-2 space-x-1">
-                      <span className="w-2 h-2 bg-red-500 rounded-full"></span>
-                      <span className="w-2 h-2 bg-yellow-500 rounded-full"></span>
-                      <span className="w-2 h-2 bg-green-500 rounded-full"></span>
-                    </div>
-                    <p className="relative w-full p-2 pr-6 font-bold text-left border-b-2 rounded shadow-xl cursor-copy bg-secondary-100 border-primary-500 hover:bg-secondary-50">
-                      Title:{" "}
-                      <span className="font-mono text-xs font-normal">
-                        Craft Beer Perfection
-                      </span>
-                      <ClipboardDocumentListIcon className="absolute w-5 h-5 ml-2 right-1 top-1 text-neutral-400" />
-                    </p>
-                    <p className="relative w-full p-2 pr-6 font-bold text-left transition border-b-2 rounded shadow-xl cursor-copy bg-secondary-100 border-primary-500 hover:bg-secondary-50">
-                      Short Desc:{" "}
-                      <span className="font-mono text-xs font-normal">
-                        Craft Beer Perfection is an exceptional product that
-                        offers a unique and unmatched taste experience.
-                      </span>
-                      <ClipboardDocumentListIcon className="absolute w-5 h-5 ml-2 right-1 top-1 text-neutral-400" />
-                    </p>
-                    <p className="relative w-full p-2 pr-6 font-bold text-left transition border-b-2 rounded shadow-xl cursor-copy bg-secondary-100 border-primary-500 hover:bg-secondary-50">
-                      Full Desc:{" "}
-                      <span className="font-mono text-xs font-normal">
-                        Indulge in the perfect sip of beer with Craft Beer
-                        Perfection. Our product is an exceptional brew that
-                        boasts of a unique and unparalleled taste experience.
-                        Every bottle of Craft Beer Perfection is made with the
-                        finest ingredients and brewed to perfection...
-                      </span>
-                      <ClipboardDocumentListIcon className="absolute w-5 h-5 ml-2 right-1 top-1 text-neutral-400" />
-                    </p>
-                    <p className="relative w-full p-2 pr-6 font-bold text-left transition border-b-2 rounded shadow-xl cursor-copy bg-secondary-100 border-primary-500 hover:bg-secondary-50">
-                      Advices:{" "}
-                      <span className="font-mono text-xs font-normal">
-                        To truly enjoy the unique and unparalleled taste
-                        experience of Craft Beer Perfection, we recommend
-                        serving it in a glass with a wide mouth...
-                      </span>
-                      <ClipboardDocumentListIcon className="absolute w-5 h-5 ml-2 right-1 top-1 text-neutral-400" />
-                    </p>
+            <div className="w-1/12 flex items-center justify-end h-full">
+              <div className="lds-ellipsis absolute right-0 ">
+                <div className="bg-primary-500"></div>
+                <div className="bg-primary-500/75"></div>
+                <div className="bg-primary-500/50"></div>
+                <div className="bg-primary-500/25"></div>
+              </div>
+            </div>
+            <div className="flex justify-center w-4/12">
+              <div className="w-5/6 rounded-xl bg-neutral-50 h-96 shadow-2xl">
+                <div className="z-10 flex flex-col justify-between w-full p-2 space-y-2 font-mono text-sm text-left pb-2 h-96">
+                  <div className="flex w-full h-2 space-x-1">
+                    <span className="w-2 h-2 bg-red-500 rounded-full"></span>
+                    <span className="w-2 h-2 bg-yellow-500 rounded-full"></span>
+                    <span className="w-2 h-2 bg-green-500 rounded-full"></span>
                   </div>
-                </Transition>
+                  <p className="relative w-full p-2 pr-6 font-bold text-left border-b-2 rounded shadow cursor-copy bg-secondary-100 border-primary-500 hover:bg-secondary-50">
+                    Title:{" "}
+                    <span className="font-mono text-xs font-normal">
+                      Craft Beer Perfection
+                    </span>
+                    <ClipboardDocumentListIcon className="absolute w-5 h-5 ml-2 right-1 top-1 text-neutral-400" />
+                  </p>
+                  <p className="relative w-full p-2 pr-6 font-bold text-left transition border-b-2 rounded shadow cursor-copy bg-secondary-100 border-primary-500 hover:bg-secondary-50">
+                    Short Desc:{" "}
+                    <span className="font-mono text-xs font-normal">
+                      Craft Beer Perfection is an exceptional product that
+                      offers a unique and unmatched taste experience.
+                    </span>
+                    <ClipboardDocumentListIcon className="absolute w-5 h-5 ml-2 right-1 top-1 text-neutral-400" />
+                  </p>
+                  <p className="relative w-full p-2 pr-6 font-bold text-left transition border-b-2 rounded shadow cursor-copy bg-secondary-100 border-primary-500 hover:bg-secondary-50">
+                    Full Desc:{" "}
+                    <span className="font-mono text-xs font-normal">
+                      Indulge in the perfect sip of beer with Craft Beer
+                      Perfection. Our product is an exceptional brew that boasts
+                      of a unique and unparalleled taste experience...
+                    </span>
+                    <ClipboardDocumentListIcon className="absolute w-5 h-5 ml-2 right-1 top-1 text-neutral-400" />
+                  </p>
+                  <p className="relative w-full p-2 pr-6 font-bold text-left transition border-b-2 rounded shadow cursor-copy bg-secondary-100 border-primary-500 hover:bg-secondary-50">
+                    Advices:{" "}
+                    <span className="font-mono text-xs font-normal">
+                      To truly enjoy the unique and unparalleled taste
+                      experience of Craft Beer Perfection...
+                    </span>
+                    <ClipboardDocumentListIcon className="absolute w-5 h-5 ml-2 right-1 top-1 text-neutral-400" />
+                  </p>
+                </div>
               </div>
             </div>
           </div>
@@ -266,16 +241,16 @@ const Home: NextPage = () => {
             </p>
           </div>
           <div className="flex justify-center w-full">
-            <div className="relative z-20 flex flex-col items-center justify-center w-1/3 h-48 text-3xl font-medium overflow-y-clip bg-primary-500 text-neutral-50">
+            <div className="relative z-20 flex shadow-2xl flex-col items-center justify-center w-1/3 h-48 text-3xl font-medium overflow-y-clip bg-primary-500 text-neutral-50">
               <ChatBubbleBottomCenterTextIcon className="w-20 h-20 mb-2" />
               <div className="flex items-center px-8">
                 Fine-tune your descriptions
-                <div className="absolute w-12 top-0 h-48 bg-primary-500 translate-y-[87px] -right-4 rotate-12"></div>
+                <div className="absolute w-12 top-0 h-48 bg-primary-500 shadow-2xl translate-y-[87px] -right-4 rotate-12"></div>
                 <div className="absolute w-12 top-0 h-48 bg-primary-500 -translate-y-[87px] -right-4 -rotate-12"></div>
                 <div className="absolute top-0 w-12 h-48 bg-primary-500 rounded-2xl -left-4"></div>
               </div>
             </div>
-            <div className="relative z-10 flex flex-col items-center justify-center w-1/3 h-48 pl-12 text-3xl font-medium overflow-y-clip bg-secondary-500 text-neutral-50">
+            <div className="relative z-10 flex flex-col shadow-2xl items-center justify-center w-1/3 h-48 pl-12 text-3xl font-medium overflow-y-clip bg-secondary-500 text-neutral-50">
               <Cog6ToothIcon className="w-20 h-20 mb-2" />
               <div className="flex items-center px-8">
                 Find product specs easily
@@ -283,7 +258,7 @@ const Home: NextPage = () => {
                 <div className="absolute w-16 top-0 h-48 bg-secondary-500 -translate-y-[87px] -right-4 -rotate-12"></div>
               </div>
             </div>
-            <div className="relative z-0 flex flex-col items-center justify-center w-1/3 h-48 pl-12 pr-4 text-3xl font-medium overflow-y-clip bg-tertiary-500 text-neutral-50">
+            <div className="relative z-0 flex flex-col shadow-2xl items-center justify-center w-1/3 h-48 pl-12 pr-4 text-3xl font-medium overflow-y-clip bg-tertiary-500 text-neutral-50">
               <RocketLaunchIcon className="w-20 h-20 mb-2" />
               <div className="flex items-center px-8">
                 Enhance your pictures like a pro

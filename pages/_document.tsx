bae938f641ed1b246499e27e4c8e83825324ef32
silentdany <dagnicourt.a@gmail.com>
2023-06-06 13:11:39 +0200

    feat: add og-image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,51 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import { NextSeo } from "next-seo";
+import { useRouter } from "next/router";
 
 class MyDocument extends Document {
   render() {
+    const router = useRouter();
     return (
       <Html lang="en" className="scroll-smooth" data-theme="depikt">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            href={`${router.basePath}/apple-touch-icon.png`}
+            key="apple"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href={`${router.basePath}/favicon-32x32.png`}
+            key="icon32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href={`${router.basePath}/favicon-16x16.png`}
+            key="icon16"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href={`${router.basePath}/android-chrome-32x32.png`}
+            key="icon32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="512x512"
+            href={`${router.basePath}/android-chrome-512x512.png`}
+            key="icon16"
+          />
+          <link
+            rel="icon"
+            href={`${router.basePath}/favicon.ico`}
+            key="favicon"
+          />
           <NextSeo
             title="depikt - Generate Product Informations From Pictures"
             description="Introducing depikt, the ultimate AI-powered tool for e-shop owners, managers and more! Say goodbye to manual product information creation. With depikt, simply upload your product pictures and let our AI generate all the relevant details instantly."
@@ -19,6 +58,14 @@ class MyDocument extends Document {
               description:
                 "Introducing depikt, the ultimate AI-powered tool for e-shop owners, managers and more! Say goodbye to manual product information creation. With depikt, simply upload your product pictures and let our AI generate all the relevant details instantly.",
               site_name: "depikt",
+              images: [
+                {
+                  url: `${router.basePath}/og-image.jpg`,
+                  width: 1200,
+                  height: 630,
+                  alt: "depikt - Generate Product Informations From Pictures ",
+                },
+              ],
             }}
             twitter={{
               handle: "@MajorBaguette",

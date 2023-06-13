c69e009f9c3e46a542c8b90f8e669ab49e8a4ff7
a.dagnicourt <dagnicourt.a@gmail.com>
2023-06-13 22:00:25 +0200

    set minimal repo for error fix

--- prisma/migrations/20230520221355_optionnal_outputs/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Description" ALTER COLUMN "outputTitle" DROP NOT NULL,
-ALTER COLUMN "outputShortDesc" DROP NOT NULL,
-ALTER COLUMN "outputFullDesc" DROP NOT NULL,
-ALTER COLUMN "outputAdvice" DROP NOT NULL;
